e16f0e17fb798f273c0e8d5dbfb552d5095fff5a
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-03-03 02:15:15 +0330

    commit message

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,10 +1,20 @@
 // accordion
 
-let accordionBtn2 = Array.from(document.getElementsByClassName('accordionBtn2'));
+function setupAccordion() {
+    let accordionBtn2 = Array.from(document.getElementsByClassName('accordionBtn2'));
 
-accordionBtn2.forEach((item)=>{
-    item.addEventListener('click', function () {
-        item.nextElementSibling.classList.toggle('active');
-        item.classList.toggle('active');
-    })
-})
+    if (window.innerWidth > 1071) {
+        accordionBtn2.forEach((item) => {
+            item.addEventListener('click', function () {
+                item.nextElementSibling.classList.toggle('active');
+                item.classList.toggle('active');
+            });
+        });
+    }
+}
+
+// اجرای تابع در ابتدا
+setupAccordion();
+
+// اجرای تابع در صورت تغییر اندازه صفحه
+window.addEventListener('resize', setupAccordion);
