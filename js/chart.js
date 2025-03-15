1f24894c3a7fa99ea20cee4774ad9dbf809c6fdb
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-03-16 03:07:35 +0330

    commit message

--- js/chart.js
+++ js/chart.js
@@ -1,6 +1,13 @@
 Highcharts.chart('container', {
     chart: {
-        type: 'column'
+        type: 'column',
+        style: {
+            fontFamily: 'pelak-Regular' // استفاده از فونت سفارشی
+        },
+        zoomType: 'x', // فعال‌سازی زوم افقی
+        scrollablePlotArea: {
+            // minWidth: 1000 // حداقل عرض برای فعال‌سازی اسکرول
+        }
     },
     title: {
         text: ''
@@ -10,17 +17,23 @@ Highcharts.chart('container', {
             ''
     },
     xAxis: {
-        categories: ['USA', 'China', 'Brazil', 'EU', 'Argentina', 'India'],
+        categories: ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور','مهر','آبان','آذر','دی','بهمن','اسفند'],
         crosshair: true,
         accessibility: {
             description: 'Countries'
+        },
+        min: 0, // شروع از اولین دسته
+        max: 10, // نمایش تنها ۵ دسته اول
+        scrollbar: {
+            enabled: true // فعال‌سازی اسکرول
         }
     },
     yAxis: {
         min: 0,
         title: {
-            text: ''
-        }
+            text: '' // عنوان محور Y خالی می‌شود
+        },
+        margin: 0 // حذف فاصله اضافی
     },
     tooltip: {
         valueSuffix: ' (1000 MT)'
@@ -28,17 +41,28 @@ Highcharts.chart('container', {
     plotOptions: {
         column: {
             pointPadding: 0.2,
-            borderWidth: 0
+            borderWidth: 0,
+            borderRadius: 10, // گرد کردن همه گوشه‌ها
         }
+    },
+    legend: {
+        enabled: true, // فعال‌سازی legend
+        verticalAlign: 'top', // انتقال legend به بالای نمودار
+        align: 'right', // تراز افقی legend به وسط
+        y: 0 // تنظیم فاصله از بالای نمودار (اختیاری)
     },
     series: [
         {
-            name: 'Corn',
-            data: [387749, 280000, 129000, 64300, 54000, 34300]
+            name: '1403',
+            data: [387749, 280000, 129000, 64300, 54000, 34300,45321, 140000, 10000, 140500, 19500, 113500],
+            pointWidth: 13, // عرض ستون‌ها را به 13 پیکسل تنظیم می‌کند
+            color: '#5D80FB'
         },
         {
-            name: 'Wheat',
-            data: [45321, 140000, 10000, 140500, 19500, 113500]
+            name: '1402',
+            data: [45321, 140000, 10000, 140500, 19500, 113500,387749, 280000, 129000, 64300, 54000, 34300],
+            pointWidth: 13, // عرض ستون‌ها را به 13 پیکسل تنظیم می‌کند
+            color:'#73CCFF'
         }
     ]
 });
